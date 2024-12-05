08bdc96cb51740342d1530e81b9ae5adfa881a9e
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2024-12-05 11:15:33 -0400

    Pruebas para obtener fizz o buzz

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,12 @@
 function obtenerFizzBuzz(numero) {
-    if (numero % 3 === 0) {
-      return 'fizz';
-    } 
+  let resultado = numero;
+  if (numero % 3 === 0) {
+    resultado = "Fizz";
   }
+  else if (numero % 5 === 0) {
+    resultado = "Buzz";
+  }
+  return resultado
+}
+
+export default obtenerFizzBuzz;
--- src/fizzBuzz.test.js
+++ src/fizzBuzz.test.js
@@ -1,7 +1,15 @@
 import obtenerFizzBuzz from "./fizzBuzz.js";
 
 describe("obtener fizz buzz para un numero", () => {
-  it("deberia obtener fizz para un numero divisible entre 3", () => {
-    expect(obtenerFizzBuzz(3)).toEqual("fizz");
+  it("deberia devolver el mismo numero", () => {
+    expect(obtenerFizzBuzz(1)).toEqual(1);
+  });
+  
+  it("deberia devolver Fizz si es dvisible entre 3", () => {
+    expect(obtenerFizzBuzz(3)).toEqual('Fizz');
+  });
+  
+  it("deberia devolver Buzz si es dvisible entre 5", () => {
+    expect(obtenerFizzBuzz(5)).toEqual('Buzz');
   });
 });
